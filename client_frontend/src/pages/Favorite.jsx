8f17d26a5175a837c657f8e963ab29dff907bfc4
Hannah-Moon <59512737+Hannah-Moon@users.noreply.github.com>
2024-05-29 16:01:59 -0400

    render: favorite page presentation

--- client_frontend/src/pages/Favorite.jsx
+++ client_frontend/src/pages/Favorite.jsx
@@ -1,7 +1,8 @@
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import FavoriteBanner from "../components/FavoriteBanner/FavoriteBanner.jsx";
-import "./Home.css";
+// import "./Home.css";
+import "../components/Products/Products.css";
 import { useContext } from "react";
 import { CoffeeContext } from "../context-and-reducer/CoffeeContext.jsx";
 import FavoriteCoffeeCard from "../components/FavoriteCoffeeCard/FavoriteCoffeeCard.jsx";
@@ -12,14 +13,27 @@ const Favorite = () => {
     <>
       <Header />
       <FavoriteBanner />
-      <div className="flex flex-row items-center lg:grid lg:grid-cols-3 px-8 mt-10 mb-10">
+      {/* <div className="flex flex-row items-center lg:grid lg:grid-cols-3 px-8 mt-10 mb-10">
         {coffees.map((card, i) => (
           <FavoriteCoffeeCard key={i} favoriteCard={card} />
         ))}
-      </div>
-
+      </div> */}
       {/* Test code from Product component to store cards. */}
+      {/* The below code works. */}
+      <section className="products-wrapper">
+        <div className="paddings innerWidth products-container">
+          <div className="flexColStart products-head">
+            <span className="eyebrowText">Your Personal Choice</span>
+            <span className="primaryText">Explore Your Saved Aromas</span>
+          </div>
 
+          <div className="products-grid">
+            {coffees.map((card, i) => (
+              <FavoriteCoffeeCard key={i} favoriteCard={card} />
+            ))}
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
